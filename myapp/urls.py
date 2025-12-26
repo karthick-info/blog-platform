0bd791a1b74b5511343e1f56fdade4336df2591c
karthick-info <bkarthick054@gmail.com>
2025-12-26 18:43:53 +0530

    Auto-redirect root URL to blog

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,7 +5,10 @@ from django.conf.urls import handler404
 from myapp.views import custom_404
 
 handler404 = 'myapp.views.custom_404'  # Update this line
+from django.views.generic import RedirectView
+
 urlpatterns = [
+    path('', RedirectView.as_view(url='/blog/', permanent=True)), # Redirect root to blog
     path("blog/",include("blog.urls")),
     path('admin/', admin.site.urls),
 ]
